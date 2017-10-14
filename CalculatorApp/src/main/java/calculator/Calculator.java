b3723f808e46698161bd0bffe641e6e66b014c98
Kevin Smith <kjsmith.dev@gmail.com>
2017-10-13 19:18:33 -0700

    Developed Calculator.java Class fields and methods

--- CalculatorApp/src/main/java/calculator/Calculator.java
+++ CalculatorApp/src/main/java/calculator/Calculator.java
@@ -1,6 +1,187 @@
 package calculator;
+/**
+ * This class preforms calculator functions for Calculator application.
+ * Calculator.java
+ */
 
+/**
+ * This class accepts the inputs from CacluatorUI and process the data and updates the text for the calculator display.
+ * @author Kevin Smith
+ * @version 1.0
+ */
 public class Calculator
 {
+    private static Double previousNumber = 0.0;
+    private static Double currentNumber = 0.0;
+    private static int numericPlaceValue = 0;
+    private static String currentOperand = "";
 
+    /**
+     * This method takes string input from the GUI and processes the input according to type of input data.
+     * @param input String - Button inputs from CalculatorUI GUI
+     */
+    public static void addInput(String input)
+    {
+        switch(input)
+        {
+            case "0" : addDouble(0.0);
+                break;
+            case "1" : addDouble(1.0);
+                break;
+            case "2" : addDouble(2.0);
+                break;
+            case "3" : addDouble(3.0);
+                break;
+            case "4" : addDouble(4.0);
+                break;
+            case "5" : addDouble(5.0);
+                break;
+            case "6" : addDouble(6.0);
+                break;
+            case "7" : addDouble(7.0);
+                break;
+            case "8" : addDouble(8.0);
+                break;
+            case "9" : addDouble(9.0);
+                break;
+            case "+" : addOperand(input);
+                break;
+            case "-" : addOperand(input);
+                break;
+            case "*" : addOperand(input);
+                break;
+            case "/" : addOperand(input);
+                break;
+            case "sin" : addOperand(input);
+                break;
+            case "cos" : addOperand(input);
+                break;
+            case "tan" : addOperand(input);
+                break;
+            case "Enter" : preformCalculation();
+                break;
+            case "C" : clearCalculator();
+                break;
+            default: System.out.println("default switch occurred");
+                break;
+        }
+    }
+
+    /**
+     * This method returns the current value of the calculator display value.
+     * @return String - of current display text value.
+     */
+    public static String getDisplayLabel()
+    {
+        return new Double(currentNumber).toString();
+    }
+
+    private static void addDouble(Double keyPressNumber)
+    {
+        if(numericPlaceValue > 0)
+        {
+            currentNumber = currentNumber * 10 + keyPressNumber;
+        }
+        else
+        {
+            currentNumber = keyPressNumber;
+        }
+        numericPlaceValue++;
+    }
+
+    private static void addOperand(String keyPressOperand)
+    {
+        currentOperand = keyPressOperand;
+
+        if(currentOperand.equals("sin") || currentOperand.equals("cos") || currentOperand.equals("tan"))
+        {
+            preformCalculation();
+        }
+        else if(previousNumber == 0.0)
+        {
+            previousNumber = currentNumber;
+            currentNumber = 0.0;
+            numericPlaceValue = 0;
+        }
+        else
+        {
+            preformCalculation();
+        }
+    }
+
+    private static void preformCalculation()
+    {
+        Double answer;
+        if(!currentOperand.equals(""))
+        {
+            if(previousNumber != 0.0)
+            {
+                switch (currentOperand)
+                {
+                    case "+" :
+                        answer = previousNumber + currentNumber;
+                        currentNumber = answer;
+                        previousNumber = 0.0;
+                        numericPlaceValue = 0;
+                        break;
+                    case "-" :
+                        answer = previousNumber - currentNumber;
+                        currentNumber = answer;
+                        previousNumber = 0.0;
+                        numericPlaceValue = 0;
+                        break;
+                    case "*" :
+                        answer = previousNumber * currentNumber;
+                        currentNumber = answer;
+                        previousNumber = 0.0;
+                        numericPlaceValue = 0;
+                        break;
+                    case "/" :
+                        answer = previousNumber / currentNumber;
+                        currentNumber = answer;
+                        previousNumber = 0.0;
+                        numericPlaceValue = 0;
+                        break;
+                    default :
+                        System.out.println("preform calculation default switch condition occurred");
+                        break;
+                }
+            }
+            else
+            {
+                switch (currentOperand)
+                {
+                    case "sin" :
+                        answer = Math.sin(Math.toRadians(currentNumber));
+                        currentNumber = answer;
+                        break;
+                    case "cos" :
+                        answer = Math.cos(Math.toRadians(currentNumber));
+                        currentNumber = answer;
+                        break;
+                    case "tan" :
+                        answer = Math.tan(Math.toRadians(currentNumber));
+                        currentNumber = answer;
+                        break;
+                    default :
+                        System.out.println("preform calculation default switch condition occurred");
+                        break;
+                }
+            }
+            //clear operand after calculation
+            currentOperand = "";
+        }
+        else
+        {
+            System.out.println("there were not operands");
+        }
+    }
+
+    private static void clearCalculator()
+    {
+        previousNumber = 0.0;
+        currentNumber = 0.0;
+        currentOperand = "";
+        numericPlaceValue = 0;
+    }
 }
--- CalculatorApp/src/main/java/ui/CalculatorUI.java
+++ CalculatorApp/src/main/java/ui/CalculatorUI.java
@@ -3,12 +3,15 @@ package ui;
  *  This Class handles the user interface elements, such as the buttons of the calculator application as well
  *  as the display window.
  */
+import calculator.Calculator;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -19,16 +22,17 @@ import javafx.stage.Stage;
  */
 public class CalculatorUI extends Application
 {
-    private static final int COLUMN_CONSTRAINT = 30;
+    private static final int COLUMN_CONSTRAINT = 50;
     private static final int HGAP = 5;
     private static final int VGAP = 5;
-    private static final int SCENE_WIDTH = 250;
-    private static final int SCENE_HEIGHT = 220;
+    private static final int SCENE_WIDTH = 325;
+    private static final int SCENE_HEIGHT = 250;
     private static final int GRID_TOP_PADDING = 20;
     private static final int GRID_RIGHT_PADDING = 60;
     private static final int GRID_BOTTOM_PADDING = 20;
     private static final int GRID_LEFT_PADDING = 60;
     private GridPane gridPane = new GridPane();
+    private Label displayText = new Label();
 
     /**
      * This method starts the calculator application.
@@ -50,31 +54,36 @@ public class CalculatorUI extends Application
         gridPane.setPadding(new Insets(GRID_TOP_PADDING, GRID_RIGHT_PADDING, GRID_BOTTOM_PADDING, GRID_LEFT_PADDING));
         gridPane.getColumnConstraints().addAll(new ColumnConstraints(COLUMN_CONSTRAINT), new ColumnConstraints(COLUMN_CONSTRAINT), new ColumnConstraints(COLUMN_CONSTRAINT));
 
-        addButtonToScene("7", 0, 0, 1, 1, 30);
-        addButtonToScene("8", 1, 0, 1, 1, 30);
-        addButtonToScene("9", 2, 0, 1, 1, 30);
-        addButtonToScene("+", 3, 0, 1, 1, 50);
-        addButtonToScene("4", 0, 1, 1, 1, 30);
-        addButtonToScene("5", 1, 1, 1, 1, 30);
-        addButtonToScene("6", 2, 1, 1, 1, 30);
-        addButtonToScene("-", 3, 1, 1, 1, 30);
-        addButtonToScene("1", 0, 2, 1, 1, 30);
-        addButtonToScene("2", 1, 2, 1, 1, 30);
-        addButtonToScene("3", 2, 2, 1, 1, 30);
-        addButtonToScene("*", 3, 2, 1, 1, 30);
-        addButtonToScene("0", 0, 3, 1, 1, 30);
-        addButtonToScene("Enter", 1, 3, 3, 1, 65);
-        addButtonToScene("/", 3, 3, 1, 1, 30);
+        addButtonToScene("sin", 0, 0, 1, 1,50);
+        addButtonToScene("cos", 1, 0, 1, 1,50);
+        addButtonToScene("tan", 2, 0, 1, 1,50);
+        addButtonToScene("C", 3, 0, 1, 1,50);
+        addButtonToScene("7", 0, 1, 1, 1, 50);
+        addButtonToScene("8", 1, 1, 1, 1, 50);
+        addButtonToScene("9", 2, 1, 1, 1, 50);
+        addButtonToScene("+", 3, 1, 1, 1, 50);
+        addButtonToScene("4", 0, 2, 1, 1, 50);
+        addButtonToScene("5", 1, 2, 1, 1, 50);
+        addButtonToScene("6", 2, 2, 1, 1, 50);
+        addButtonToScene("-", 3, 2, 1, 1, 50);
+        addButtonToScene("1", 0, 3, 1, 1, 50);
+        addButtonToScene("2", 1, 3, 1, 1, 50);
+        addButtonToScene("3", 2, 3, 1, 1, 50);
+        addButtonToScene("*", 3, 3, 1, 1, 50);
+        addButtonToScene("0", 0, 4, 1, 1, 50);
+        addButtonToScene("Enter", 1, 4, 2, 1, 110);
+        addButtonToScene("/", 3, 4, 1, 1, 50);
 
-        Label textDisplay = new Label();
-        textDisplay.setAlignment(Pos.CENTER_RIGHT);
-        textDisplay.setPrefWidth(Double.MAX_VALUE);
-        textDisplay.setPadding(new Insets(5));
+        displayText.setText(Calculator.getDisplayLabel());
+        displayText.setId("textDisplay");
+        displayText.setAlignment(Pos.CENTER_RIGHT);
+        displayText.setPrefWidth(Double.MAX_VALUE);
+        displayText.setPadding(new Insets(5));
 
         //Border border = new Border(new BorderStroke(Paint.valueOf("black"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
         //textDisplay.setBorder(border);
 
-        gridPane.add(textDisplay, 0, 4, 4, 1);
+        gridPane.add(displayText, 0, 5, 4, 1);
 
         Scene scene = new Scene(gridPane, SCENE_WIDTH, SCENE_HEIGHT);
 
@@ -86,8 +95,24 @@ public class CalculatorUI extends Application
     private void addButtonToScene(String label,Integer columnIndex, Integer rowIndex, Integer colSpan, Integer rowSpan, Integer preferWidth)
     {
         Button btn = new Button(label);
+        btn.setId(label);
         btn.setPadding(new Insets(5, 10, 5, 10));
         btn.setPrefWidth(preferWidth);
+        btn.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent event)
+            {
+                Calculator.addInput(btn.getId());
+                displayText.setText(Calculator.getDisplayLabel());
+            }
+        });
         gridPane.add(btn, columnIndex, rowIndex, colSpan, rowSpan);
+
+    }
+
+    private void setLableText()
+    {
+
     }
 }
